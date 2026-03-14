f0007f7d68f16a1bd9744e0fc40b176e81cd177d
ghoshrishit2009-ops <ghoshrishit2009@gmail.com>
2026-03-15 00:04:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+window.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('signupBtn').addEventListener('click', function() {
+        window.location.href = 'signup.html';
+    });
+});
